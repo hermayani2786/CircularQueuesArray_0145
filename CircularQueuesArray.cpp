6c381dc8a4d8f0e9ef86ce7eb399dd35f3b18444
hermayani2786 <herma.yani2786@gmail.com>
2026-05-28 11:12:30 +0700

    menambahkan case 2

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -148,3 +148,8 @@ int main()
                 q.insert();
                 break;
             }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
